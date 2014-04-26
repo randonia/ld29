7a07c1ee177b3db4531110804e1cfafcd74b90f2
Ryan Andonian <ryanandonian@gmail.com>
2014-04-25 22:33:30 -0700

    Added base gamescreen class. Has init, update(delta), draw(ctx), and (unload) functions

---
+++ src/GameScreen.js
@@ -0,0 +1,11 @@
+// Game Screen object
+
+function GameScreen(){};
+
+GameScreen.prototype.init = function(){};
+
+GameScreen.prototype.update = function(delta){};
+
+GameScreen.prototype.draw = function(ctx){};
+
+GameScreen.prototype.unload = function(){};
